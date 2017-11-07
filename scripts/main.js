6168e1ee0f0582c9e78384a9e44fc6183d811b7e
Katriel Coleman <TheFunkyL@Katriels-MacBook-Pro.local>
2017-11-07 15:12:13 -0600

    Initial Commit.. totally thought I did this already.

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+// alert();
+//results[0].lfs[]
+//https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=da548e54a4034fb1b2446c4ec786cfbd&query=batman
+$('#getButton').click(function(){
+	$('#wordContainer').html('');
+	var search = $('#searchResults').val();
+
+	$.getJSON('https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=da548e54a4034fb1b2446c4ec786cfbd&query='+ search, function(data){
+		console.log(data);
+
+		
+		for (var i = 0; i < data.results.length; i++) {
+
+			//$('#wordContainer').html(JSON.stringify(data[0].lfs[i]));
+			$('#wordContainer').append('<h1 class="button">' + data.results[i].display_title + '<br>' +  data.results[i].summary_short + '</h1>' );
+
+			// $('#movieName').append(data.results[i].display_title);
+			// $('#summary').append(data.results[i].summary_short);
+			
+			console.log(data);
+		}
+
+		$('.button').click(function(){
+			$(this).toggleClass('open');
+			$('#wordContainer').toggleClass('openContainer');
+			// $('.description').css('display','inline-block');
+		});
+	});
+});
+	
