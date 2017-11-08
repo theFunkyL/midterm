c87b082403884bebd40bbb2700fd7498b68581e9
Katriel Coleman <TheFunkyL@Katriels-MacBook-Pro.local>
2017-11-07 18:43:34 -0600

    Second Commit Pre Debugging

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,9 @@
 //results[0].lfs[]
 //https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=da548e54a4034fb1b2446c4ec786cfbd&query=batman
 $('#getButton').click(function(){
-	$('#wordContainer').html('');
+	$('.wordContainer').html('');
 	var search = $('#searchResults').val();
+	$('.openContainer').toggleClass(".wordContainer");
 
 	$.getJSON('https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=da548e54a4034fb1b2446c4ec786cfbd&query='+ search, function(data){
 		console.log(data);
@@ -12,7 +13,7 @@ $('#getButton').click(function(){
 		for (var i = 0; i < data.results.length; i++) {
 
 			//$('#wordContainer').html(JSON.stringify(data[0].lfs[i]));
-			$('#wordContainer').append('<h1 class="button">' + data.results[i].display_title + '<br>' +  data.results[i].summary_short + '</h1>' );
+			$('.wordContainer').append('<h1 class="button">' + data.results[i].display_title + '<br>' +  data.results[i].summary_short + '</h1>' );
 
 			// $('#movieName').append(data.results[i].display_title);
 			// $('#summary').append(data.results[i].summary_short);
@@ -22,7 +23,7 @@ $('#getButton').click(function(){
 
 		$('.button').click(function(){
 			$(this).toggleClass('open');
-			$('#wordContainer').toggleClass('openContainer');
+			$('.wordContainer').toggleClass('openContainer');
 			// $('.description').css('display','inline-block');
 		});
 	});
